ad86b54193743cb6f35864a7ab992a9dcef6085f
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-03 15:38:15 -0700

    [react] create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Welcome from './components/Welcome.js'
 import About from './components/About'
 import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Welcome />
       <About />
       <Footer />
