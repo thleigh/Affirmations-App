2c872b994d4bf568908de75160c174a6452169cd
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-08 11:31:10 -0700

    [resources and about] create basic resources page and setup about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import margaret from '../assets/margaret.png'
+import steven from '../assets/steven.png'
+import tanner from '../assets/tanner.png'
+import github from '../assets/githublogo.png'
+import linkedin from '../assets/linkedinlogo.png'
 
 const About = () => {
     return (
-        <div>
+        <>
+        <div className="aboutContainer">
             <div className="aboutMain">
                 <h3 className="aboutTitle">About</h3>
                 <p className="aboutBody">Affirmations is an app created by <strong>Margaret Huang</strong>, <strong>Steven Michaud</strong>, and   
@@ -14,6 +20,35 @@ const About = () => {
                 </p>
             </div>
         </div>
+        <div>
+            <div className="aboutGrid">
+                <div className="margaret">
+                    <img src={margaret} alt="Margaret"/>
+                    <p>Margaret Huang</p>
+                    <div className="logoLinks">
+                        <a href="https://github.com/margaret-jihua" target="_blank"><img src={github}/></a>
+                        <a href="https://www.linkedin.com/in/margaret-jihua/" target="_blank"><img src={linkedin}/></a>
+                    </div>
+                </div>
+                <div className="steven">
+                    <img src={steven} alt="Steven"/>
+                    <p>Steven Michaud</p>
+                    <div className="logoLinks">
+                        <a href="https://github.com/Michaudsr" target="_blank"><img src={github}/></a>
+                        <a href="https://www.linkedin.com/in/steven-michaud/" target="_blank"><img src={linkedin}/></a>
+                    </div>
+                </div>
+                <div className="tanner">
+                    <img src={tanner} alt="Tanner"/>
+                    <p>Tanner Leigh</p>
+                    <div className="logoLinks">
+                        <a href="https://github.com/thleigh" target="_blank"><img src={github}/></a>
+                        <a href="https://www.linkedin.com/in/tannerhleigh/" target="_blank"><img src={linkedin}/></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
