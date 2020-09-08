858173e29af2088a91a3792133232e72784d1694
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-08 10:41:53 -0700

    [about page and phone number] add phone number design and about page design

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,9 +3,16 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
-            <p>Occaecat pariatur nulla sunt labore officia voluptate dolor tempor in velit sunt. Dolore est laborum proident 
-                reprehenderit ut pariatur consequat sit. Duis minim quis fugiat laboris nostrud. Esse laboris sint minim ut.</p>
+            <div className="aboutMain">
+                <h3 className="aboutTitle">About</h3>
+                <p className="aboutBody">Affirmations is an app created by <strong>Margaret Huang</strong>, <strong>Steven Michaud</strong>, and   
+                 <strong> Tanner Leigh</strong>, through React and MongoDB that encourages and informs users that are struggling or 
+                just curious about suicide and depression. It is simple by nature, allowing it to 
+                be accessible to everyone and beautiful to the eye to make it less intimidating to 
+                the average user. Affirmations strives to be a safe place for all and keeps the mental
+                 and physical safety of its users as its highest priority.
+                </p>
+            </div>
         </div>
     )
 }
