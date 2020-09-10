ad6daa4511d9639905646cbfee18dc13e032e7a8
margare-jihua <margaret.jihua@gmail.com>
2020-09-10 09:14:41 -0700

    [NewAffirmation] setup and running

--- frontend/src/components/newAffirmation.js
+++ frontend/src/components/newAffirmation.js
@@ -5,9 +5,19 @@ const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const NewAffirmation = () => {
 
-    let [modalShowNewQuote, setModalShowNewQuote] = useState(false);
     let [quote, setQuote] = useState('')
     let [author, setAuthor] = useState('')
+    let [show, setShow] = useState(false)
+    let [submitted, setSubmitted] = useState(false)
+
+    const handleShow = () => setShow(true)
+    
+    const handleClose = () => { 
+        setShow(false)
+        setSubmitted(false)
+        setQuote('')
+        setAuthor('')
+    }
 
     const handleQuote = (e) => {
         setQuote(e.target.value)
@@ -19,54 +29,60 @@ const NewAffirmation = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(e.target, "Submitted!");
+        axios.post(`${REACT_APP_SERVER_URL}/api/affirmations`,{"quote": quote, "author": author})
+        .then(response => {
+            console.log(response.data);
+            setSubmitted(true)
+        })
+        .catch(err => console.log(err))
     }
-
-    function NewQuoteModal(props) {
-        return (
-          <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Add A New Affirmation
-              </Modal.Title>
-            </Modal.Header>
-                <Modal.Body>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <div className="form-group">
-                        <label htmlFor="quote">Quote: </label>
-                        <input name="quote" type="text" className="form-control"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="author">Author: </label>
-                        <input name="author" type="text" className="form-control"/>
-                    </div>
-                    <button type="submit" className="btn btn-info">Submit</button>
-                </form>
-                </Modal.Body>
-                <Modal.Footer>
-                <Button className="btn-info" onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-          </Modal>
-        );
-    }
-
 
     return (
         <>
             <li className="nav-item">
-                <a href="#" className="nav-link text-info" variant="primary" onClick={() => setModalShowNewQuote(true)}>
+                <a href="#" className="nav-link text-info" variant="primary" onClick={handleShow}>
                     Add Affirmation
                 </a>
             </li>
-            <NewQuoteModal
-                show={modalShowNewQuote}
-                onHide={() => setModalShowNewQuote(false)}
-            />
+
+            <Modal show={show} onHide={handleClose} size="lg" centered aria-labelledby="contained-modal-title-vcenter">
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        New Affirmation
+                    </Modal.Title>
+                </Modal.Header>
+                {
+                    !submitted ?
+                    <>
+                    <Form onSubmit={(e) => handleSubmit(e)}>
+                        <Modal.Body>
+                                <div className="form-group">
+                                <label htmlFor="quote">Quote: </label>
+                                <input name="quote" type="text" value={quote} onInput={handleQuote} className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="author">Author: </label>
+                                <input name="author" type="text" value={author} onInput={handleAuthor} className="form-control"/>
+                            </div>
+                            
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button className="btn-info" type="submit">Submit</Button>
+                        </Modal.Footer>
+                    </Form>
+                    </>
+                    :
+                    <>
+                    <Modal.Body>
+                        <p>{quote}</p>
+                        <p>{author}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className="btn-info" onClick={handleClose}>Close</Button>
+                    </Modal.Footer>
+                    </>
+                }
+            </Modal>
         </>
     )
 }
