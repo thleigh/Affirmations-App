460d4f0397f7b7f567d9c7dd113c0082ba8be4cb
margare-jihua <margaret.jihua@gmail.com>
2020-09-10 16:32:22 -0700

    [Profile] Display liked quotes

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,18 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
+import {Row, Col} from 'react-bootstrap'
+import axios from 'axios';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Profile = (props) => {
+
+    let [affirmations, setAffirmations] = useState([]);
+    let [likedQuotes, setLikedQuotes] = useState('')
+
     window.flash(`Welcome back, ${props.user.name}` , 'success')
+
+    useEffect(() => {
+        axios.get(`${REACT_APP_SERVER_URL}/api/affirmations`)
+        .then(response => {
+            setAffirmations(response.data);
+        });
+    }, []);
+
+    const hasLikedQuotes = () => {
+
+        let affirmationList = affirmations.map((affirmation, idx) => {
+            if (affirmation.likes.includes(props.user.id)) {
+                return (
+                    <>
+                        <h6>{affirmation.quote} -- {affirmation.author}</h6>
+                    </>
+                )
+            }            
+        })
+        
+        if (affirmationList.length) {
+
+            return (
+                <div className="liked-quotes">
+                    <h4>Affirmations You Liked:</h4>
+                    <ul>{affirmationList}</ul>
+                </div>
+            )
+            
+        } else{
+
+            return(            
+                <h5>
+                    Check out our <a href="/">homepage</a> to like some affirmations!
+                </h5>                
+            )
+        }               
+    }
+
     const userData = props.user ? 
-    (<div>
-        <h1>Profile</h1>
-        <p><strong>Name: </strong>{props.user.name}</p>
-        <p><strong>Email: </strong>{props.user.email}</p>
-        <p><strong>ID: </strong>{props.user.id}</p>
-        <p><strong>Volunteer: </strong>{props.user.volunteer ? "Yes" : "No"}</p>
-        {props.user.volunteer ? <p><strong>Phone Number: </strong>{props.user.phoneNumber}</p> : null}
-    </div>) : <h4>Loading...</h4>
-    console.log(props.user)
+    (
+    <>
+        <Row>
+            <Col sm={4}>
+                <h1>Profile🌼</h1> 
+                <p><strong>Name: </strong>{props.user.name}</p>
+                <p><strong>Email: </strong>{props.user.email}</p>
+                <p><strong>Volunteer: </strong>{props.user.volunteer ? "Yes" : "No"}</p>
+                {props.user.volunteer ? <p><strong>Phone Number: </strong>{props.user.phoneNumber}</p> : null}
+            </Col>
+
+            <Col sm={8}>
+                {hasLikedQuotes()}
+            </Col>
+        </Row>
+    </>
+    ) : 
+    <h4>Loading...</h4>
 
     const errorDiv = () => {
         return (
